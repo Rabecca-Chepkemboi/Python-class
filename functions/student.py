2e1364ea281a3f1dcf6d87bf5850fd2be0b5e6c4
Chepkemboi <Chepkemboirabecca@gmail.com>
2023-06-17 16:04:06 +0300

    my pthon classwork

---
+++ functions/student.py
@@ -0,0 +1,16 @@
+def year_of_birth(name, age):
+    year = 2023-age
+    print(f"Hello{name}, you were born in {year}")
+    
+
+
+
+class Student:
+    name = "Chep"
+    age = 24
+    school = "AkiraChix"
+    nationality = "Kenyan"
+
+student1 = Student()
+student1 = Student()
+student3 = Student()  
